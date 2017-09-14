45771fed8ed4fef82a92a20d030370bf285d2a1b
DJ <dj@DJ-MacBook-Pro.local>
2017-09-13 22:16:35 -0700

    commit

---
+++ learn/dj/helloworld/src/A1.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class A1 extends Component {
+    render () {
+        return (
+            <div>
+                This is A1 Component
+            </div>   
+        );
+    }
+}
+
+export default A1;
--- learn/dj/helloworld/src/App.js
+++ learn/dj/helloworld/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import A1 from './A1.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hello World!
+        
+        <A1 />
       </div>
     );
   }
