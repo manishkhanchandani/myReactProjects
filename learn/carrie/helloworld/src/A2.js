5a1f0c6e257a4593b7e6b8dad6b22a40c30b0b39
cparecki <carrie@carrieparecki.com>
2017-09-13 22:17:46 -0700

    add A2 and A3 Components

---
+++ learn/carrie/helloworld/src/A2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class A2 extends Component {
+  render() {
+    return (
+      <div>
+	  	This is A2 component
+      </div>
+    );
+  }
+}
+
+export default A2;
--- learn/carrie/helloworld/src/App.js
+++ learn/carrie/helloworld/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import A1 from './A1.js';
+import A2 from './A2.js';
+import A3 from './A3.js';
 
 class App extends Component {
   render() {
@@ -7,7 +9,9 @@ class App extends Component {
       <div>
 	  	hello world
 		
-		<A1 />
+		<A1 />		
+		<A2 />	
+		<A3 />
       </div>
     );
   }
