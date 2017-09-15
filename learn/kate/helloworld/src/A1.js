146ebc9b3030c87ba995092eaf74757d395be561
design9992 <design9992@gmail.com>
2017-09-14 21:40:33 -0700

    A1 from kate

---
+++ learn/kate/helloworld/src/A1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class A1 extends Component {
+  render() {
+    return (
+        <div>
+            A1 component
+      </div>
+    );
+  }
+}
+
+export default A1;
