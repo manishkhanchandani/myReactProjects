727211a7cfd2d9a6ec2583363ed980abbeff8cf6
cparecki <carrie@carrieparecki.com>
2017-09-14 21:44:08 -0700

    take out merge issue

--- learn/carrie/helloworld/src/App.js
+++ learn/carrie/helloworld/src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import A1 from './A1.js';
-<<<<<<< HEAD
-=======
 import A2 from './A2.js';
 import A3 from './A3.js';
->>>>>>> 5a1f0c6e257a4593b7e6b8dad6b22a40c30b0b39
 
 class App extends Component {
   render() {
     return (
       <div>
 	  	hello world
-		
-<<<<<<< HEAD
-		<A1 />
-=======
 		<A1 />		
 		<A2 />	
 		<A3 />
->>>>>>> 5a1f0c6e257a4593b7e6b8dad6b22a40c30b0b39
       </div>
     );
   }
