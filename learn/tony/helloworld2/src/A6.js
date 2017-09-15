fe2b3a4fd8d3171af3b7ac4a20f18861a345811c
Tony Smith <tws0321@gmail.com>
2017-09-15 13:24:10 -0700

    Added more sample components A6.js thru A15.js

---
+++ learn/tony/helloworld2/src/A6.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class A6 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is A6 component</h1>
+            </div>
+        );
+    }
+}
+
+export default A6;
