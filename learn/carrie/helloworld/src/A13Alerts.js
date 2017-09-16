1b209336693713b403d30b3e42febc146126ef45
cparecki <carrie@carrieparecki.com>
2017-09-15 23:24:49 -0700

    Homework Assignment Bootstrap Components

---
+++ learn/carrie/helloworld/src/A13Alerts.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class A13Alerts extends Component {
+  render() {
+    return (
+		<div>      
+			<div className="page-header">
+				<h1>Alerts</h1>
+			  </div>
+			  <div className="alert alert-success" role="alert">
+				<strong>Well done!</strong> You successfully read this important alert message.
+			  </div>
+			  <div className="alert alert-info" role="alert">
+				<strong>Heads up!</strong> This alert needs your attention, but it's not super important.
+			  </div>
+			  <div className="alert alert-warning" role="alert">
+				<strong>Warning!</strong> Best check yo self, you're not looking too good.
+			  </div>
+			  <div className="alert alert-danger" role="alert">
+				<strong>Oh snap!</strong> Change a few things up and try submitting again.
+			  </div>
+		</div>
+    );
+  }
+}
+
+export default A13Alerts;
---
+++ learn/carrie/helloworld/src/A14ProgressBars.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class A14ProgressBars extends Component {
+  render() {
+    return (
+		<div> 
+			<div className="page-header">
+				<h1>Progress bars</h1>
+			</div>
+		</div>
+    );
+  }
+}
+
+export default A14ProgressBars;
