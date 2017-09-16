8b70413ede9f16338d1bad3d0ab2a9323732efe4
Manish Khanchandani <manishkk74@gmail.com>
2017-09-15 21:01:00 -0700

    more

--- learn/manny/helloworld/src/App.js
+++ learn/manny/helloworld/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import A1 from './A1.js';
 import A2 from './A2.js';
 
 class App extends Component {
   render() {
     return (
       <div >
-	  	<h1>Main Application</h1>
-		
+		<A1 />
 		<A2 />
       </div>
     );
