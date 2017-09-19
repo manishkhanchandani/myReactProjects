136442feecd623b977bc1ca14d49b17ce6625cd6
DJ <dj@DJ-MacBook-Pro.local>
2017-09-18 22:55:34 -0700

    Sep 18 assignments

--- learn/dj/helloworld/src/App.js
+++ learn/dj/helloworld/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-
+//import A1 from './A1.js';
+//import A3 from './A3.js';
+import A11 from './A11.js';
 
 class App extends Component {
   render() {
     return (
       <div >
-		
+        {/*<A1 /> <A3 />*/}
+        
+        <div >
+		<A11 name="Kate" age="22" gender="Female" city="San Jose" state="" country="USA" />
+		<A11 name="Carrie" age="23" gender="Female" city="Cupertino" state="" country="USA" />
+		<A11 name="DJ" age="24" gender="Male" city="San Francisco" state="" country="USA"/>
+		<A11 name="Tony" age="25" gender="Male" city="Sunnyvale" state="" country="USA"/>
+      </div>
+        
       </div>
     );
   }
