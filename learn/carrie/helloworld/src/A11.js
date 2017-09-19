7af75422f17cfaab161beaf5c2110b2584c21dad
cparecki <carrie@carrieparecki.com>
2017-09-18 21:55:24 -0700

    updating file names that were changes somehow.

---
+++ learn/carrie/helloworld/src/A11.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class A11 extends Component {
+  render() {
+    return (
+		<div>
+			<span class="name" >
+		</div>
+    );
+  }
+}
+
+export default A1;
--- learn/carrie/helloworld/src/App-9-15-17.js
+++ learn/carrie/helloworld/src/App-9-15-17.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import A1 from './A11.js';
+import A1 from './A1.js';
+import A3 from './A3.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-		<A11 />	
+		<A1 />	
+		<A3 />
       </div>
     );
   }
