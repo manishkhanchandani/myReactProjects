999e887683d5aa8bcad2ee4a1d82e96b48695e7e
cparecki <carrie@carrieparecki.com>
2017-09-18 22:12:16 -0700

    props redoing as it was not clean last time

--- learn/carrie/helloworld/src/A11.js
+++ learn/carrie/helloworld/src/A11.js
@@ -4,7 +4,7 @@ class A11 extends Component {
   render() {
     return (
 		<div>
-			<span className="label label-default">{this.props.name} - {this.props.age} / {this.props.gender} - {this.props.city} - {this.props.state} - {this.props.country}</span>
+			<span className="label label-default">{this.props.name} - {this.props.age} - {this.props.gender} - {this.props.city} - {this.props.state} - {this.props.country}</span>
 		</div>
     );
   }
--- learn/carrie/helloworld/src/App.js
+++ learn/carrie/helloworld/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div>
 		<A11 name="Kate" age="22" gender="Female" city="San Francisco" state="CA" country="USA" />
 		<A11 name="Carrie" age="23" gender="Female" city="Germantown" state="MD" country="USA" />
-		<A11 name="DJ" age="24" gender="Male" city="San Mateo" state="CA" country="USA" />
+		<A11 name="DJ" age="24" gender="Male" city="San Mateo" state="CA" country="US" />
 		<A11 name="Tony" age="25" gender="Male" city="Arlington" state="VA" country="USA" />
       </div>
     );
