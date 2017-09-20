4f164b235ffbc2bc8be7f856a11c98c9317ceca0
Manish Khanchandani <manishkk74@gmail.com>
2017-09-20 04:34:17 -0700

    more

--- learn/manny/helloworld/src/App.js
+++ learn/manny/helloworld/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import A11 from './A11.js';
 
-
 class App extends Component {
   render() {
+	  var myData = [
+				{name: 'kate', age: 23},
+				{name: 'carrie', age: 24},
+				{name: 'manny', age: 43},
+				{name: 'tony', age: 28},
+				{name: 'dj', age: 26}
+		];
+	  
+	 console.log(myData);
+	 
     return (
       <div >
-		<A11 name="Kate" age="22" gender="Female" />
-		<A11 name="Carrie" age="23" gender="Female" />
-		<A11 name="DJ" age="24" gender="Male" />
-		<A11 name="Tony" age="25" gender="Male" />
+		<A11 {...myData[0]} />
+		<A11 {...myData[1]} />
+		<A11 {...myData[2]} />
+		<A11 {...myData[3]} />
+		<A11 {...myData[4]} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
