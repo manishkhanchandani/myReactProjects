0c73ab6b24ad52f3d25bf74ec6431f87bad64b84
design9992 <design9992@gmail.com>
2017-09-19 21:45:04 -0700

    more info added to a11

--- learn/kate/helloworld/src/App.js
+++ learn/kate/helloworld/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import A1 from './A1.js';
-import A3 from './A3.js';
+import A11 from './A11.js';
 
 class App extends Component {
   render() {
     return (
       <div>
 	
-	     <A1/>
-		 <A3/>
+	     <A11 name="kate" age="20" gender="female" country="us" city="fremont"/>
+		 <A11 name="carrie" age="18" gender="female" country="us" city="fremont"/>
+		 <A11 name="dj" age="23" gender="male" country="us" city="fremont"/>
+		 <A11 name="tony" age="19" gender="male" country="us" city="fremont"/>
+		 
       </div>
     );
   }
