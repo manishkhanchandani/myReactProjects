e28e1914fdb5ef67e1a4a2dad238f5f4e3c29ef6
Dikesh <dikesh.com@gmail.com>
2017-09-21 13:54:33 -0700

    sep 20 class

--- learn/dj/helloworld/src/A11.js
+++ learn/dj/helloworld/src/A11.js
@@ -4,13 +4,13 @@ class A11 extends Component {
 	render() {
 		return (
 			<div>
-                <h1>{this.props.name}</h1>
-                <p><b>Age: </b> {this.props.age}</p>
-                <p><b>Gender: </b> {this.props.gender}</p>
-                <p><b>City: </b> {this.props.city}</p>
-                <p><b>State: </b> {this.props.state}</p>
-                <p><b>Country: </b> {this.props.country}</p>
-            </div>
+				<h1>{this.props.name}</h1>
+				<p><b>Age: </b> {this.props.age}</p>
+				<p><b>Gender: </b> {this.props.gender}</p>
+				<p><b>City: </b> {this.props.city}</p>
+				<p><b>State: </b> {this.props.state}</p>
+				<p><b>Country: </b> {this.props.country}</p>
+			</div>
 		);	
 	}
 }
