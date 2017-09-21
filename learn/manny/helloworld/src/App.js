ba733fee86e89f45853351a47645048096ea56ae
Manish Khanchandani <manishkk74@gmail.com>
2017-09-20 21:49:13 -0700

    more

--- learn/manny/helloworld/src/App.js
+++ learn/manny/helloworld/src/App.js
@@ -4,22 +4,22 @@ import A11 from './A11.js';
 class App extends Component {
   render() {
 	  var myData = [
-				{name: 'kate', age: 23},
-				{name: 'carrie', age: 24},
-				{name: 'manny', age: 43},
-				{name: 'tony', age: 28},
-				{name: 'dj', age: 26}
+				{name: 'kate1', age: 31},
+				{name: 'carrie2', age: 32},
+				{name: 'manny3', age: 33},
+				{name: 'tony4', age: 35},
+				{name: 'dj5', age: 34}
 		];
 	  
 	 console.log(myData);
 	 
     return (
-      <div >
-		<A11 {...myData[0]} />
-		<A11 {...myData[1]} />
-		<A11 {...myData[2]} />
-		<A11 {...myData[3]} />
-		<A11 {...myData[4]} />
+      <div>
+            {
+                myData.map((value, index) => {
+                    return <A11 key={index} {...value} />
+                })
+            }
       </div>
     );
   }
