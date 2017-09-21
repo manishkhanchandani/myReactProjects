31b4baa1f3153500d7b4e9b5ea22cf0b2308fafd
design9992 <design9992@gmail.com>
2017-09-20 22:21:04 -0700

    project1 folder

--- learn/kate/helloworld/src/App.js
+++ learn/kate/helloworld/src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import A11 from './A11.js';
+import Nav from './project1/Nav.js';
+import Cont from './project1/Cont.js';
+import './A2.css';
 
 class App extends Component {
   render() {
-	  var myData = [
-		  {name:"kate", age:21},
-		  {name:"carrie", age:22},
-		  {name:"manny", age:43},
-		  {name:"tony", age:30},
-		  {name:"dj", age:28}
-	
-		  ];
+	 
 	  
     return (
       <div>
-	
-	      <A11 {...myData[0]} />
-		  <A11 {...myData[1]} />
-		  <A11 {...myData[2]} />
-		  <A11 {...myData[3]} />
-		  <A11 {...myData[4]} />
-		 
+			<Nav/>
+			<Cont/>
+			
       </div>
     );
   }
