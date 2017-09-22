5bea2c77edaea7dea97c7b451aa9c2bbac3b1418
design9992 <design9992@gmail.com>
2017-09-21 21:40:05 -0700

    2 cols

--- learn/kate/helloworld/src/project1/Cont.js
+++ learn/kate/helloworld/src/project1/Cont.js
@@ -5,9 +5,9 @@ class Cont extends Component {
 		return (	
 <div className="container">
 <div className="row">
-<div className="col-md-3"><h3>Col 1</h3></div>
-<div className="col-md-6"><h3>Col 2</h3></div>
-<div className="col-md-3"><h3>Col 3</h3></div>
+<div className="col-md-4"><h3>Col 1</h3></div>
+<div className="col-md-8"><h3>Col 2</h3></div>
+
 </div>
 </div>
 		);	
