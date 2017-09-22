2ebe40c278409e74a96749c1e7ed9b205ba85bc8
Tony Smith <tws0321@gmail.com>
2017-09-21 21:46:51 -0700

    Moved container to project1 folder and fixed links

--- learn/tony/helloworld/src/App.js
+++ learn/tony/helloworld/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './project1/Navigation.js';
-import Container from './2ColumnContainer.js';
+import Container from './project/Container.js';
 import './A2.css';
 
 class App extends Component {
