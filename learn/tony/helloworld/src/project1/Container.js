1fc8f8a9855cc42df3765aab47b40901b5e542a0
Tony Smith <tws0321@gmail.com>
2017-09-21 22:28:38 -0700

    Fixed div tag class error

--- learn/tony/helloworld/src/project1/Container.js
+++ learn/tony/helloworld/src/project1/Container.js
@@ -7,15 +7,15 @@ class Container extends Component {
                                 <div className="row">
                                         <div classname="col=md-3"><h3>Search Food</h3>
                                                 <form>
-                                                        <div> className="form-group">
+                                                        <div className="form-group">
                                                                 <label>Keyword</label>
                                                                 <input type="text" className="form-control" id="keyword" placeholder="Enter Keyword" />
                                                         </div> 
-                                                        <div> className="form-group">
+                                                        <div className="form-group">
                                                                 <label>Location</label>
                                                                 <input type="text" className="form-control" id="location" placeholder="Enter Location" />
                                                         </div>   
-                                                        <div> className="form-group">
+                                                        <div className="form-group">
                                                                 <button type="submit" className="btn btn-primary form-control">Search</button>
                                                         </div>
                                                 </form>        
