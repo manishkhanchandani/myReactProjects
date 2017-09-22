a588ef7fedcb27584499085dc54fcc2f606af1dc
Tony Smith <tws0321@gmail.com>
2017-09-21 21:40:25 -0700

    Added 2 column container

--- learn/tony/helloworld/src/App.js
+++ learn/tony/helloworld/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './project1/Navigation.js';
-import Container from './project1/Container.js';
+import Container from './2ColumnContainer.js';
 import './A2.css';
 
 class App extends Component {
