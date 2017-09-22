591db40aa6b811c0f3c8a4c272d5a4b2adab2109
Tony Smith <tws0321@gmail.com>
2017-09-21 21:49:05 -0700

    Fixed reference in App.js to project1/container.js

--- learn/tony/helloworld/src/App.js
+++ learn/tony/helloworld/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './project1/Navigation.js';
-import Container from './project/Container.js';
+import Container from './project1/Container.js';
 import './A2.css';
 
 class App extends Component {
