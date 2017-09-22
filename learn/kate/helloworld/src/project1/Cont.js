f44298861f55370731d237064c4b08a83f49dd2d
design9992 <design9992@gmail.com>
2017-09-21 21:41:30 -0700

    change to 3 / 9

--- learn/kate/helloworld/src/project1/Cont.js
+++ learn/kate/helloworld/src/project1/Cont.js
@@ -5,8 +5,8 @@ class Cont extends Component {
 		return (	
 <div className="container">
 <div className="row">
-<div className="col-md-4"><h3>Col 1</h3></div>
-<div className="col-md-8"><h3>Col 2</h3></div>
+<div className="col-md-3"><h3>Col 1</h3></div>
+<div className="col-md-9"><h3>Col 2</h3></div>
 
 </div>
 </div>
