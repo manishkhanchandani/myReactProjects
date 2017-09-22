121f61bec6d4aeeb94f097031bb76a086104b730
Dikesh <dikesh.com@gmail.com>
2017-09-21 22:20:40 -0700

    class work

--- learn/dj/helloworld/src/project1/Container.js
+++ learn/dj/helloworld/src/project1/Container.js
@@ -3,13 +3,24 @@ import React, {Component} from 'react';
 class Container extends Component {
 	render() {
 		return (	
-                    <div className="container">
-                            <div className="row">
-                                    <div className="col-md-3"><h3>Col 1</h3></div>
-                                    <div className="col-md-9"><h3>Col 2</h3></div>
-                                    
-                            </div>
-                    </div>
+		<div className="container">
+			<div className="row">
+				<div className="col-md-3"><h3>Search Food</h3>
+				<form>
+					  <div className="form-group">
+						<label>Keyword</label>
+						<input type="text" className="form-control" id="keyword" placeholder="Enter Keyword" />
+					  </div>
+					  <div className="form-group">
+						<label>Location</label>
+						<input type="text" className="form-control" id="location" placeholder="Enter Location" />
+					  </div>
+					<button type="submit" className="btn btn-primary form-control">Search</button>
+				</form>
+				<div className="col-md-9"><h3>Col 2</h3></div>
+				</div>
+			</div>
+		</div>
 		);	
 	}
 }
