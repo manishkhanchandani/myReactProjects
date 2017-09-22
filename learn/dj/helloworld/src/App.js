7c2b205cce0ac4d01ebe756c755efef7c89a2338
Dikesh <dikesh.com@gmail.com>
2017-09-21 21:37:53 -0700

    sept 21 class work

--- learn/dj/helloworld/src/App.js
+++ learn/dj/helloworld/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './project1/Navigation.js';
-import Container from './project1/Container.js';
+import Container from './project1/Challange1.js';
 import './A2.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div>
            <Navigation />
-           <Container />
+           <Challange1 />
       </div>
     );
   }
