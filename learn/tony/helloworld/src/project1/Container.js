8afd4086f237221afaf3d91e49c141cacf1a92fc
Tony Smith <tws0321@gmail.com>
2017-09-22 20:45:31 -0700

    Add Search.js component, which is called from Container.js

--- learn/tony/helloworld/src/project1/Container.js
+++ learn/tony/helloworld/src/project1/Container.js
@@ -1,32 +1,19 @@
 import React, {Component} from 'react';
+import Search from './Search.js';
+
+
 
 class Container extends Component {
-	render() {
-		return (	
-                        <div className="container">
-                                <div className="row">
-                                        <div className="col-md-3"><h3>Search Food</h3>
-                                                <form>
-                                                        <div className="form-group">
-                                                                <label>Keyword</label>
-                                                                <input type="text" className="form-control" id="keyword" placeholder="Enter Keyword" />
-                                                        </div> 
-                                                        <div className="form-group">
-                                                                <label>Location</label>
-                                                                <input type="text" className="form-control" id="location" placeholder="Enter Location" />
-                                                        </div>   
-                                                        <div className="form-group">
-                                                                <button type="submit" className="btn btn-primary form-control">Search</button>
-                                                        </div>
-                                                </form>        
-                                        </div>
-                                </div>
-                                <div className="col-md-9"><h3>Col 2</h3></div>         
-                        </div>
-                              
-        
-		);	
-	}
+    render() {
+        return (	
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3"><Search/></div>
+                    <div className="col-md-9"><h3>Col 2</h3></div>
+                </div>          
+            </div>
+        );	
+    }
 }
 
-export default Container
+export default Container;
