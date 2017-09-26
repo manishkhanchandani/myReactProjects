52a6d6a691b576bfcb9298ebf791a68b6960201d
Dikesh <dikesh.com@gmail.com>
2017-09-25 22:11:28 -0700

    Sep 25 2017

--- learn/dj/helloworld/src/App.js
+++ learn/dj/helloworld/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'; 
 
 import Home from './project2/Home.js';
 import About from './project2/About.js';
@@ -11,9 +11,13 @@ class App extends Component {
 		<Router>
 		  <div>
 		  	<h1>Header</h1>
-			<p><a href="/">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a></p>
+			<p><Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link></p>
 			<hr />
 			
+				<Route exact={true} path="/" component={Home} />
+				<Route exact={true} path="/about" component={About} />
+				<Route exact={true} path="/contact" component={Contact} />
+		
 			<hr />
 			<h1>Footer</h1>
 		  </div>
