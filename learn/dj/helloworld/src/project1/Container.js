d87b44ede600a32c6dd58881f7b2a2f58cef1334
Dikesh <dikesh.com@gmail.com>
2017-09-25 21:25:59 -0700

    updated

--- learn/dj/helloworld/src/project1/Container.js
+++ learn/dj/helloworld/src/project1/Container.js
@@ -1,26 +1,19 @@
 import React, {Component} from 'react';
+{/*}import SearchForm from './project1/SearchForm.js';*/}
 
 class Container extends Component {
 	render() {
 		return (	
-		<div className="container">
+	<div className="container">
 			<div className="row">
-				<div className="col-md-3"><h3>Search Food</h3>
-				<form>
-					  <div className="form-group">
-						<label>Keyword</label>
-						<input type="text" className="form-control" id="keyword" placeholder="Enter Keyword" />
-					  </div>
-					  <div className="form-group">
-						<label>Location</label>
-						<input type="text" className="form-control" id="location" placeholder="Enter Location" />
-					  </div>
-					<button type="submit" className="btn btn-primary form-control">Search</button>
-				</form>
-				<div className="col-md-9"><h3>Col 2</h3></div>
+				<div className="col-md-3">
+			{/*<SearchForm />*/}
+				</div>
+				<div className="col-md-9">
+					<h3>Col 2</h3>
 				</div>
 			</div>
-		</div>
+	</div>
 		);	
 	}
 }
