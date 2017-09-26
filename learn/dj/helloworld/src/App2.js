4e9d96dd09683c3840fa5d29e673305a28a44c8b
Dikesh <dikesh.com@gmail.com>
2017-09-25 21:26:55 -0700

    updated

---
+++ learn/dj/helloworld/src/App2.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Navigation from './project1/Navigation.js';
+import Container from './project1/Container.js';
+import './A2.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+           <Navigation />
+           <Container />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
