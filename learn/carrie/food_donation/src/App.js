467ca1c0a01182f115b14e93cfa6fbcdc4ec83e0
cparecki <carrie@carrieparecki.com>
2017-09-29 22:34:22 -0700

    files from class

--- learn/carrie/food_donation/src/App.js
+++ learn/carrie/food_donation/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     <nav className="navbar navbar-inverse navbar-static-top">
       <div className="container">
         <div className="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span className="sr-only">Toggle navigation</span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
@@ -30,21 +30,21 @@ class App extends Component {
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
             <li className="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
               <ul className="dropdown-menu">
                 <li><a href="#">Action</a></li>
                 <li><a href="#">Another action</a></li>
                 <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
+                <li role="separator" className="divider"></li>
                 <li className="dropdown-header">Nav header</li>
                 <li><a href="#">Separated link</a></li>
                 <li><a href="#">One more separated link</a></li>
               </ul>
             </li>
           </ul>
-          <ul class="nav navbar-nav navbar-right">
+          <ul className="nav navbar-nav navbar-right">
             <li><a href="../navbar/">Default</a></li>
-            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
+            <li className="active"><a href="./">Static top <span className="sr-only">(current)</span></a></li>
             <li><a href="../navbar-fixed-top/">Fixed top</a></li>
           </ul>
         </div>
@@ -58,7 +58,6 @@ class App extends Component {
 				  <Route exact={true} path="/myaccount" component={MyAccount} />
 				  <Route exact={true} path="/chat" component={Chat} />
 				  
-			 <h1>footer</h1>
    		   </div>
 		  </Router>
     );
