1ccf63534541b09a872bf2e1c01f45c7e0b81cc7
Manish Khanchandani <manishkk74@gmail.com>
2017-10-03 22:06:16 -0700

    s

---
+++ projects/food_donation/src/components/Results.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Results extends Component {
+	render() {
+		return (
+			<div className="media">
+			  <div className="media-left">
+				<a href="">
+				  <img className="media-object myImage" src="https://img.buzzfeed.com/buzzfeed-static/static/2014-06/23/15/campaign_images/webdr07/26-traditional-indian-foods-that-will-change-your-1-7312-1403550756-15_big.jpg" alt="..." />
+				</a>
+			  </div>
+			  <div className="media-body">
+				<h4 className="media-heading">Media heading</h4>
+				<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+			  </div>
+			</div>
+		);
+	}
+}
+
+export default Results;
