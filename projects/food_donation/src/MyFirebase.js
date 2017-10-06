1792b3a3beb3bddb5c209f45e25be7a66a1fca7d
Manish Khanchandani <manishkk74@gmail.com>
2017-10-05 22:10:17 -0700

    z

---
+++ projects/food_donation/src/MyFirebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBhpHK-ve2s0ynnr8og8Zx0S69ttEFpDKk",
+	authDomain: "project100-fe20e.firebaseapp.com",
+	databaseURL: "https://project100-fe20e.firebaseio.com",
+	projectId: "project100-fe20e",
+	storageBucket: "project100-fe20e.appspot.com",
+	messagingSenderId: "674827815611"
+};
+
+export const FirebaseConstant = {
+	basePath: '/food_donation'	
+};
+
+export const firebaseApp = firebase.initializeApp(config);
+
+export const firebaseDatabase = firebase.database();
