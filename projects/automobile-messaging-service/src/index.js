8db93928e09aefea733a442fab84c414a0df4978
Tony Smith <tws0321@gmail.com>
2017-10-09 10:32:41 -0700

    Got project up-to-date with class

--- projects/automobile-messaging-service/src/index.js
+++ projects/automobile-messaging-service/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import * as firebase from 'firebase';
+import {firebaseApp, firebaseDatabase} from './MyFireBase.js';
+
+firebaseApp.auth().onAuthStateChanged((user) => {
+	console.log('user is ', user);
+	if (user) {
+		console.log('user is logged in ');	
+		var obj = {};
+		obj.email = user.email;
+		obj.displayName = user.displayName;
+		obj.photoURL = user.photoURL;
+		obj.uid = user.uid;
+		obj.profile_uid = user.providerData[0].uid;
+		obj.providerId = user.providerData[0].providerId;
+		console.log('obj2: ', obj);
+	} else {
+		console.log('user is logged out');	
+	}
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
