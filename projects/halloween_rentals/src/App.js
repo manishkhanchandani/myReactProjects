14587f029a153a4be0d564285287fb0e9d83f3cf
Tony Smith <tws0321@gmail.com>
2017-10-09 10:32:54 -0700

    Got project up-to-date with class

--- projects/halloween_rentals/src/App.js
+++ projects/halloween_rentals/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'; 
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import Nav from './components/Nav.js';
 
 import Home from './components/Home.js';
 import Create from './components/Create.js';
@@ -12,45 +13,8 @@ class App extends Component {
   render() {
     return (
 	  <Router>
-      <div>
-	  	<nav className="navbar navbar-inverse navbar-static-top">
-		  <div className="container">
-			<div className="navbar-header">
-			  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-				<span className="sr-only">Toggle navigation</span>
-				<span className="icon-bar"></span>
-				<span className="icon-bar"></span>
-				<span className="icon-bar"></span>
-			  </button>
-			  <a className="navbar-brand" href="#">Halloween Rentals</a>
-			</div>
-			<div id="navbar" className="navbar-collapse collapse">
-			  <ul className="nav navbar-nav">
-				<li className="active"><a href="#">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<li className="dropdown">
-				  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-				  <ul className="dropdown-menu">
-					<li><a href="#">Action</a></li>
-					<li><a href="#">Another action</a></li>
-					<li><a href="#">Something else here</a></li>
-					<li role="separator" className="divider"></li>
-					<li className="dropdown-header">Nav header</li>
-					<li><a href="#">Separated link</a></li>
-					<li><a href="#">One more separated link</a></li>
-				  </ul>
-				</li>
-			  </ul>
-			  <ul className="nav navbar-nav navbar-right">
-				<li><a href="../navbar/">Default</a></li>
-				<li className="active"><a href="./">Static top <span className="sr-only">(current)</span></a></li>
-				<li><a href="../navbar-fixed-top/">Fixed top</a></li>
-			  </ul>
-			</div>
-		  </div>
-		</nav>
-	
+			<div>
+				<Nav />
 			
         <Route exact={true} path="/" component={Home} />
         <Route exact={true} path="/create" component={Create} />
@@ -58,8 +22,7 @@ class App extends Component {
         <Route exact={true} path="/delete/:id" component={Delete} />
         <Route exact={true} path="/myaccount" component={MyAccount} />
         <Route exact={true} path="/chat" component={Chat} />
-
-      </div>
+			</div>
 	  </Router>
     );
   }
--- projects/halloween_rentals/src/components/Home.js
+++ projects/halloween_rentals/src/components/Home.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Autocomplete from 'react-google-autocomplete';
 
+import Results from './Results.js';
+
 class Home extends Component {
 	render() {
 		return (
@@ -9,20 +11,60 @@ class Home extends Component {
 					<div className="col-md-3">
 						<h3>Search</h3>
 						<form>
-							<div className="form-group">
-								<label>Keyword</label>
-								<input type="text" className="form-control" placeholder="Enter any Keyword" />
-							</div>
-
-							<Autocomplete className="form-control" onPlaceSelected={(place) => {
-								console.log(place);  
+						  <div className="form-group">
+							<label>Keyword</label>
+							<input type="text" className="form-control" placeholder="Enter any Keyword" />
+						  </div>
+						  
+						  <Autocomplete className="form-control addressBox" onPlaceSelected={(place) => {
+							  
+								if (!place.formatted_address) {
+									alert('please choose the address');
+									return null;
+								}	
+								
+								console.log(place);
+								var componentForm = {
+									locality: 'long_name',
+									administrative_area_level_1: 'short_name',
+									administrative_area_level_2: 'long_name',
+									country: 'short_name',
+								  };
+								
+								var obj = {};
+								obj.formatted_address = place.formatted_address;
+								obj.lat = place.geometry.location.lat();
+								obj.lng = place.geometry.location.lng();
+								
+								for (var i = 0; i < place.address_components.length; i++) {
+								  var addressType = place.address_components[i].types[0];
+								  if (componentForm[addressType]) {
+									var val = place.address_components[i][componentForm[addressType]];
+									obj[addressType] = val;
+								  }
+								}
+								
+								
+								console.log(obj);
 							}} types={['geocode']} />
-							<button type="submit" className="btn btn-primary form-control">Search</button>
+						  <br />
+						  <button type="submit" className="btn btn-primary form-control">Search</button>
 						</form>
 					</div>
-
 					<div className="col-md-9">
 						<h3>Results</h3>
+						
+						<Results />
+						<Results />
+						<Results />
+						<Results />
+						<Results />
+						<Results />
+						<Results />
+						<Results />
+						
+						
+						
 					</div>
 				</div>
 			</div>
