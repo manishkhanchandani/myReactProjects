144b10f2daf0bfa40c95b8a1955348465770990c
Manish Khanchandani <manishk@chegg.com>
2017-10-10 16:00:49 -0700

    more

--- projects/food_donation_test/src/actions/MyAction.js
+++ projects/food_donation_test/src/actions/MyAction.js
@@ -1,4 +1,7 @@
 
+import * as firebase from 'firebase';
+import {firebaseApp, firebaseDatabase} from '../MyFirebase.js';
+
 export const loggedIn = (params) => {
 	return {
 		type: 'LOGGEDIN',
@@ -15,4 +18,28 @@ export const loggedOut = () => {
 	return {
 		type: 'LOGGEDOUT'	
 	};	
+};
+
+export const googleLogin = () => {
+    return {
+		type: 'GOOGLELOGIN',
+        payload: new Promise((resolve, reject) => {
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebaseApp.auth().signInWithPopup(provider).then(function(result) {
+                var obj = {};
+                obj.email = result.user.email;
+                obj.displayName = result.user.displayName;
+                obj.photoURL = result.user.photoURL;
+                obj.uid = result.user.uid;
+                obj.profile_uid = result.user.providerData[0].uid;
+                obj.providerId = result.user.providerData[0].providerId;
+                console.log('obj in glogin: ', obj);
+                resolve(obj);
+            }).catch(function(error) {
+                reject(error);
+            });
+        })
+	};
+    
+    
 };
--- projects/food_donation_test/src/components/Nav.js
+++ projects/food_donation_test/src/components/Nav.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'; 
+import {connect} from 'react-redux';
 
 import * as firebase from 'firebase';
 import {firebaseApp, firebaseDatabase} from '../MyFirebase.js';
+
+import {googleLogin} from '../actions/MyAction.js';
 
 class Nav extends Component {
 	
 	googleLogin(e) {
 		e.preventDefault();
+        this.props.callGoogleLogin();
+        /*
 		var provider = new firebase.auth.GoogleAuthProvider();
 		firebaseApp.auth().signInWithPopup(provider).then(function(result) {
 			var obj = {};
@@ -18,18 +23,26 @@ class Nav extends Component {
 			obj.profile_uid = result.user.providerData[0].uid;
 			obj.providerId = result.user.providerData[0].providerId;
 			console.log('obj: ', obj);
-		});
+		});*/
 	}
 	
 	signOut(e) {
 		e.preventDefault();
 		firebaseApp.auth().signOut().then(function() {
-												   
+
 		});
 		
 	}
 	
 	render() {
+        console.log('nav props: ', this.props);
+        var strLoggedIn = [];
+        if (this.props.myReducer.displayName) {
+            strLoggedIn.push(<li key="1" className="myNavPos">{this.props.myReducer.displayName}</li>);
+            strLoggedIn.push(<li key="2"><a href="" onClick={this.signOut.bind(this)}>SignOut</a></li>);
+        } else {
+            strLoggedIn.push(<li key="3"><a href="" onClick={this.googleLogin.bind(this)}>Google Login</a></li>);
+        }
 		return (
 			<nav className="navbar navbar-inverse navbar-static-top">
 		  <div className="container">
@@ -40,7 +53,7 @@ class Nav extends Component {
 				<span className="icon-bar"></span>
 				<span className="icon-bar"></span>
 			  </button>
-			  <Link className="navbar-brand" to="/">Food Donation</Link>
+			  <Link className="navbar-brand" to="/">Food Donation 1</Link>
 			</div>
 			<div id="navbar" className="navbar-collapse collapse">
 			  <ul className="nav navbar-nav">
@@ -49,11 +62,7 @@ class Nav extends Component {
 				<li className="dropdown">
 				  <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User <span className="caret"></span></a>
 				  <ul className="dropdown-menu">
-					<li><a href="" onClick={this.googleLogin.bind(this)}>Google Login</a></li>
-					<li><a href="">Facebook Login</a></li>
-					<li><a href="">Twitter Login</a></li>
-					<li><a href="">GitHub Login</a></li>
-					<li><a href="" onClick={this.signOut.bind(this)}>SignOut</a></li>
+					{strLoggedIn}
 				  </ul>
 				</li>
 			  </ul>
@@ -69,4 +78,19 @@ class Nav extends Component {
 	}
 }
 
-export default Nav;
+const mapStateToProps = (state) => {
+  return {
+    myReducer: state.MyReducer
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    callGoogleLogin: () => {
+      dispatch(googleLogin());
+    }
+  }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
