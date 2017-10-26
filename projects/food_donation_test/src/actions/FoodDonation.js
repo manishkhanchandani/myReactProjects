2cad644dd0472861de1aba27d6ac961f7e0aec1e
Manish Khanchandani <manishk@chegg.com>
2017-10-26 06:06:12 -0700

    more

--- projects/food_donation_test/src/actions/FoodDonation.js
+++ projects/food_donation_test/src/actions/FoodDonation.js
@@ -1,12 +1,16 @@
 import * as firebase from 'firebase';
 import {firebaseDatabase, FirebaseConstant} from '../MyFirebase.js';
 
+import {dynamicSort, dynamicSortMultiple} from '../utilities/functions.js';
+
 export const createPost = (params) => {
 	return {
 		type: 'CREATE_POST',
        payload: new Promise((resolve, reject) => {
+            params.created_dt= firebase.database.ServerValue.TIMESTAMP;
             var url = FirebaseConstant.basePath + '/data/posts';
             var postId = firebaseDatabase.ref(url).push(params).key;
+            firebaseDatabase.ref(url).child(postId).child('postId').set(postId);
             var current = firebase.database.ServerValue.TIMESTAMP;
             var state = params.location.administrative_area_level_1;
             var country = params.location.country;
@@ -36,16 +40,16 @@ export const createPost = (params) => {
                     var tag = tags[i].trim();
                     console.log('tag is ', tag);
                     
-                    var urlCountry = FirebaseConstant.basePath + '/data/tags/'+tag+'/country';
+                    urlCountry = FirebaseConstant.basePath + '/data/tags/'+tag+'/country';
                     firebaseDatabase.ref(urlCountry).child(country).child(postId).set(current);
                     
-                    var urlState = FirebaseConstant.basePath + '/data/tags/'+tag+'/state';
+                    urlState = FirebaseConstant.basePath + '/data/tags/'+tag+'/state';
                     firebaseDatabase.ref(urlState).child(country).child(state).child(postId).set(current);
         
-                    var urlCounty = FirebaseConstant.basePath + '/data/tags/'+tag+'/county';
+                    urlCounty = FirebaseConstant.basePath + '/data/tags/'+tag+'/county';
                     firebaseDatabase.ref(urlCounty).child(country).child(state).child(county).child(postId).set(current);
                     
-                    var urlCity = FirebaseConstant.basePath + '/data/tags/'+tag+'/city';
+                    urlCity = FirebaseConstant.basePath + '/data/tags/'+tag+'/city';
                     firebaseDatabase.ref(urlCity).child(country).child(state).child(county).child(city).child(postId).set(current);
                 }
             }
@@ -62,8 +66,9 @@ export const browsePost = () => {
         type: '',
         payload: new Promise((resolve, reject) => {
             var url = FirebaseConstant.basePath + '/data/posts';
-            var ref = firebaseDatabase.ref(url);
+            var ref = firebaseDatabase.ref(url).orderByChild('created_dt').limitToLast(500);
             ref.on('value', function(snapshot) {
+                var result = snapshot.val();
               console.log('snapshot is ', snapshot.val());
             });
         })
