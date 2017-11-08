943b5e145f516dc178bfb14cd01d62e7273bcfcb
Manish Khanchandani <manishkk74@gmail.com>
2017-11-07 22:31:55 -0800

    d

--- projects/food_donation/src/components/Home.js
+++ projects/food_donation/src/components/Home.js
@@ -1,12 +1,8 @@
 import React, {Component} from 'react';
 
 import {connect} from 'react-redux';
-import {browseFoodDonation} from '../actions/FoodDonationAction.js';
 
 import ResultContainer from './ResultContainer.js';
-import {firebaseDatabase, FirebaseConstant} from '../MyFirebase.js';
-import {Redirect} from 'react-router-dom';
-
 import SearchForm from './SearchForm.js';
 
 class Home extends Component {
@@ -17,57 +13,6 @@ class Home extends Component {
 		this.state = {
 			subObjects: {}	
 		};
-	}
-	
-	processLoop(key)
-	{
-		var subURL = FirebaseConstant.basePath + '/data/posts/'+key;
-		var subRef = firebaseDatabase.ref(subURL);
-		var tmp = this.state.subObjects;
-		subRef.once('value', (subSnapshot) => {
-			tmp[key] = subSnapshot.val();	
-			this.setState({subObjects: tmp});
-			
-			this.props.func2(this.state.subObjects);
-		});
-	}
-
-	componentDidMount() {
-		var obj = this.props.match.params;
-		console.log('obj is ', obj);
-		var url = FirebaseConstant.basePath + '/data';
-		if (obj.keyword && obj.lat && obj.lng) {
-			//do something
-		} else if (obj.lat && obj.lng) {
-			//do something
-			if (obj.boundary === 'county') {
-				url = url + '/' + obj.boundary + '/' + obj.country + '/' + obj.state + '/' + obj.county;
-			} else if (obj.boundary === 'city') {
-				url = url + '/' + obj.boundary + '/' + obj.country + '/' + obj.state + '/' + obj.county + '/' + obj.city;
-			} else if (obj.boundary === 'state') {
-				url = url + '/' + obj.boundary + '/' + obj.country + '/' + obj.state;
-			} else if (obj.boundary === 'country') {
-				url = url + '/' + obj.boundary + '/' + obj.country;
-			}
-		} else if (obj.keyword) {
-			//do something
-		} else {
-			//home page will go here	
-			this.props.func1();
-			return;
-		}
-		
-		console.log('url is ', url);
-		
-		var ref = firebaseDatabase.ref(url);
-		ref.on('value', (snapshot) => {
-			var records = snapshot.val();
-			if (!records) return null;
-			
-			for (var key in records) {
-				this.processLoop(key);
-			}
-		});
 	}
 
 	render() {
@@ -97,23 +42,4 @@ const mapStateToProps = (state) => {
 	}	
 };
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		func1: () => {
-			//api or firebase or any url which backend developer has provide to you		
-			var url = FirebaseConstant.basePath + '/data/posts';
-			//create a reference of above url
-			var ref = firebaseDatabase.ref(url);
-			ref.on('value', function(snapshot) {
-				dispatch(browseFoodDonation(snapshot.val()));	
-			});
-			
-		},
-		
-		func2: (details) => {
-			dispatch(browseFoodDonation(details));	
-		}
-	};	
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps)(Home);
