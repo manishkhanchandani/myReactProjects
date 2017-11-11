e39f8c79ccb0c821281dc67cd1bb102a3d2c8b52
Manish Khanchandani <manishkk74@gmail.com>
2017-11-10 22:41:40 -0800

    cc

--- projects/food_donation/src/components/ResultContainer.js
+++ projects/food_donation/src/components/ResultContainer.js
@@ -11,7 +11,9 @@ class ResultContainer extends Component {
 		super(props);
 		
 		this.state = {
-			pageNumber: 1	
+			pageNumber: 1,
+			filterTerm: null,
+			sortingField: null
 		}
 	}
 	
@@ -19,8 +21,21 @@ class ResultContainer extends Component {
 		this.setState({pageNumber: page});
 	}
 	
+	dynamicSort(property) {
+		var sortOrder = 1;
+		if(property[0] === "-") {
+			sortOrder = -1;
+			property = property.substr(1);
+		}
+		return function (a,b) {
+			var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+			return result * sortOrder;
+		}
+	}
+	
 	render() {
 		console.log('props are ', this.props);
+		console.log('state are ', this.state);
 		if (!this.props.data) {
 			return null;	
 		}
@@ -33,6 +48,28 @@ class ResultContainer extends Component {
 			}
 			myArray.push(this.props.data[key]);
 		}
+		
+		//FILTER RESULTS
+		if (this.state.filterTerm) {
+			myArray = myArray.filter((record) => {
+				if (!record.title) record.title = '';
+				if (!record.description) record.description = '';
+				if (!record.location.administrative_area_level_1) record.location.administrative_area_level_1 = '';
+				if (!record.location.administrative_area_level_2) record.location.administrative_area_level_2 = '';
+				if (!record.location.country) record.location.country = '';
+				if (!record.location.formatted_address) record.location.formatted_address = '';
+				if (!record.location.locality) record.location.locality = '';
+				if (!record.id) record.id = '';
+				return (record.title.toLowerCase().indexOf(this.state.filterTerm.toLowerCase())	>= 0 || record.description.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0 || record.tags.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0 || record.location.administrative_area_level_1.toLowerCase().indexOf(this.state.filterTerm.toLowerCase())	>= 0 || record.location.administrative_area_level_2.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0 || record.location.country.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0 || record.location.formatted_address.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0 || record.location.locality.toLowerCase().indexOf(this.state.filterTerm.toLowerCase())	>= 0 || record.id.toLowerCase().indexOf(this.state.filterTerm.toLowerCase()) >= 0);  
+				
+			});	
+		}		
+		
+		//SORTING RESULTS
+		if (this.state.sortingField) {
+			myArray.sort(this.dynamicSort(this.state.sortingField));	
+		}
+		
 		
 		const maxRows = 3;
 		const pageNum = this.state.pageNumber - 1;
@@ -56,7 +93,15 @@ class ResultContainer extends Component {
 		
 		
 		return (
-			<div>				
+			<div>
+				<div className="row">
+					<div className="col-md-6">
+						<input type="text" placeholder="Filter" className="form-control" onChange={(e) => {this.setState({filterTerm: e.target.value, pageNumber: 1});}} />
+					</div>
+					<div className="col-md-6">
+						<input type="text" placeholder="Sorting" className="form-control" onChange={(e) => {this.setState({sortingField: e.target.value, pageNumber: 1});}} />
+					</div>
+				</div>
 				{
 					myArrayConverted.map((value, index) => {
 						return	<Results record={value} key={index}  /> 			 
