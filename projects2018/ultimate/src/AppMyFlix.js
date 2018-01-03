51531ae05c14cefd25fae5471363f737c80020b4
Manish Khanchandani <manishkk74@gmail.com>
2018-01-02 22:20:54 -0800

    s

---
+++ projects2018/ultimate/src/AppMyFlix.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppMyFlix extends Component {
+  render() {
+    return (
+      <div>
+        AppMyFlix
+      </div>
+    );
+  }
+}
+
+export default AppMyFlix;
--- projects2018/ultimate/src/Layouts/OnlineRxLayout.js
+++ projects2018/ultimate/src/Layouts/OnlineRxLayout.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+import React, {Component} from 'react';
+
+class OnlineRxLayout extends Component {
+	render() {
+		return (
+			<div>
+				OnlineRxLayout
+			</div>
+		);
+	}
+}
+
+export default OnlineRxLayout;
