ed988bf79a9f90265f167a28b86575e370c4b03e
shuai <design9992@gmail.com>
2018-01-03 22:09:37 -0800

    nav multi

--- projects2018/ultimate/src/Layouts/BookingLayout.js
+++ projects2018/ultimate/src/Layouts/BookingLayout.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import NavMulti from '../Booking/nav-multi/NavMulti.js';
 
 class BookingLayout extends Component {
     
 	render() {
 		return (
              <div>
+                <NavMulti/>
                 BookingLayout
              </div>
 		);
