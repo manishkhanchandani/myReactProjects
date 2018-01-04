4a3b39688c5f4dfa1ea28d30427c6563c0f9f9ec
shuai <design9992@gmail.com>
2018-01-03 22:40:03 -0800

    add router to booking

--- projects2018/ultimate/src/Layouts/BookingLayout.js
+++ projects2018/ultimate/src/Layouts/BookingLayout.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import NavMulti from '../Booking/nav-multi/NavMulti.js';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class BookingLayout extends Component {
     
 	render() {
 		return (
+            <Router>
              <div>
-                <NavMulti/>
                 BookingLayout
+                <NavMulti/>
+                
+                <Route exact={true} path="/" component={Home} />
+				<Route exact={true} path="/create" component={Create} />
+            
              </div>
+            </Router>
 		);
 	}
 }
--- projects2018/ultimate/src/index.js
+++ projects2018/ultimate/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import {Provider} from 'react-redux';
 import store from './store.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
 
