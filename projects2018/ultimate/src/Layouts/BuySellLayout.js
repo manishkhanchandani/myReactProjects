b56bf63dd650f8b9f06d0f0f7f86f98aeecf5043
Tony Smith <tws0321@gmail.com>
2018-01-03 21:48:26 -0800

    Adding default code to BuySellLayout

--- projects2018/ultimate/src/Layouts/BuySellLayout.js
+++ projects2018/ultimate/src/Layouts/BuySellLayout.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+import React, {Component} from 'react';
+
+class BuySellLayout extends Component {
+	render() {
+		return (
+			<div>
+				Buy and Sell Layout
+			</div>
+		);
+	}
+}
+
+export default BuySellLayout;
